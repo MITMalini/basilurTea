c6df4d366a9bcf567d31148687f8d8e913bc01a3
Malini Jayaweera <MaliniJ@millenniumitesp.com>
2023-03-09 15:17:40 +0530

    backend commit

---
+++ BACKEND/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const operatorsRoutes= require('./routes/operators.routes.js');
+const cors =require('cors');
+
+require('dotenv').config();
+
+// app config
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+//routes
+app.use('/api/operators', operatorsRoutes);
+
+// connect to db
+const dotenv = require('dotenv');
+dotenv.config({path:'config.env'});
+
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('mongodb connection successful');
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+//port
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
+
---
+++ BACKEND/controllers/operators.controller.js
@@ -0,0 +1,105 @@
+const Operator = require('../models/operators.model.js');
+
+//get all Operators
+
+const getOperators = async (req, res) => {
+  const operators = await Operator.find();
+
+  if (!operators) {
+    return res.status(404).json({ message: 'No any available operators found' });
+  } else {
+    return res.status(200).json(operators);
+  }
+};
+
+//get / serach a single Operator
+
+const getOperator = async (req, res) => {
+  const { id } = req.params;
+
+  const operator = await Operator.findById({ _id: id });
+
+  if (!operator) {
+    return res.status(404).json({ message: 'No operator found' });
+  } else {
+    return res.status(200).json(operator);
+  }
+};
+
+//add new Operator
+
+const addOperator = async (req, res) => {
+  const { nic, operator_name, index_number, z_score, subject_stream, email, degree, password } =
+    req.body;
+
+  try {
+    const operator = await Operator.create({
+      nic,
+      operator_name,
+      index_number,
+      z_score,
+      subject_stream,
+      email,
+      degree,
+      password,
+    });
+
+    return res.status(200).json(operator);
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ message: e.message });
+  }
+};
+
+//delete a Operator
+
+const deleteOperator = async (req, res) => {
+  const { id } = req.params;
+
+  const operator = await Operator.findOneAndDelete({ _id: id });
+
+  if (!operator) {
+    return res.status(400).json({ error: 'No such operator' });
+  } else {
+    return res.status(200).json({ message: 'Operator deleted successfully' });
+  }
+};
+
+//update a Operator
+
+const updateOperator = async (req, res) => {
+  const { id } = req.params;
+
+  const operator = await Operator.findOneAndUpdate({ _id: id }, { ...req.body });
+
+  if (!operator) {
+    return res.status(400).json({ error: 'No such operator' });
+  } else {
+    return res.status(200).json(operator);
+  }
+};
+
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const data = { email: email, password: password }
+  return Operator.find(data)
+    .then((operator) => {
+      if (operator.length > 0) {
+        
+        return res.status(200).json({ operator })
+      } else {
+        return res.status(404).json({ "message": "operator not found" })
+      }
+    })
+    .catch(err => { console.log("login failed " + err) })
+}
+
+module.exports = {
+    getOperators,
+    getOperator,
+    addOperator,
+    deleteOperator,
+    updateOperator,
+  login,
+};
